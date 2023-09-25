c5f712ea8d97c36ff0627fc25faae5ebd8e58150
Nicholas Shipes <nshipes@urban10.com>
2023-09-25 16:17:00 -0700

    chore(example): wrap map in ErrorBoundary, test mount/unmount toggle

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,12 +1,34 @@
+'use client';
+
 import { createRoot } from 'react-dom/client';
-// import ReactMapboxMap from './ReactMapboxMap';
+import { useState } from 'react';
+import { WeatherLayer } from '@aerisweather/react-mapsgl';
+import { ErrorBoundary } from 'react-error-boundary';
+import ReactMapboxMap from './ReactMapboxMap';
 import ReactMapGLMap from './ReactMapGLMap';
 // import MapboxMap from './MapboxMap';
 // import MaplibreMap from './MaplibreMap';
-// import GoogleMap from './GoogleMap';
+import GoogleMap from './GoogleMap';
 // import LeafletMap from './LeafletMap';
 
 const container = document.querySelector('#app');
 const root = createRoot(container!);
 
-root.render(<ReactMapGLMap />);
+const App = () => {
+    const [isHidden, setIsHidden] = useState(false);
+
+    return (
+        <>
+            <div style={{ marginBottom: '8px' }}>
+                <button onClick={() => setIsHidden(!isHidden)}>{isHidden ? 'Show' : 'Hide'} Map</button>
+            </div>
+            <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                {!isHidden && (
+                    <ReactMapGLMap />
+                )}
+            </ErrorBoundary>
+        </>
+    );
+};
+
+root.render(<App />);

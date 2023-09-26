eea93fac01b9adae6a43b1178e5c5b4059e48198
Nicholas Shipes <nshipes@urban10.com>
2023-09-26 13:26:26 -0700

    improvement(example): add map/library type switch property

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -6,16 +6,46 @@ import { WeatherLayer } from '@aerisweather/react-mapsgl';
 import { ErrorBoundary } from 'react-error-boundary';
 import ReactMapboxMap from './ReactMapboxMap';
 import ReactMapGLMap from './ReactMapGLMap';
-// import MapboxMap from './MapboxMap';
-// import MaplibreMap from './MaplibreMap';
+import MapboxMap from './MapboxMap';
+import MaplibreMap from './MaplibreMap';
 import GoogleMap from './GoogleMap';
-// import LeafletMap from './LeafletMap';
+import LeafletMap from './LeafletMap';
 
 const container = document.querySelector('#app');
 const root = createRoot(container!);
 
-const App = () => {
+interface AppProps {
+    type: 'mapbox' | 'maplibre' | 'google' | 'leaflet' | 'react-map-gl' | 'react-mapbox-gl';
+}
+
+const App = ({ type, ...rest }: Partial<AppProps>) => {
     const [isHidden, setIsHidden] = useState(false);
+
+    let MapSlot = MapboxMap;
+
+    switch (type) {
+        case 'maplibre': {
+            MapSlot = MaplibreMap;
+            break;
+        }
+        case 'google': {
+            MapSlot = GoogleMap;
+            break;
+        }
+        case 'leaflet': {
+            MapSlot = LeafletMap;
+            break;
+        }
+        case 'react-map-gl': {
+            MapSlot = ReactMapGLMap;
+            break;
+        }
+        case 'react-mapbox-gl': {
+            MapSlot = ReactMapboxMap;
+            break;
+        }
+        default:
+    }
 
     return (
         <>
@@ -24,11 +54,11 @@ const App = () => {
             </div>
             <ErrorBoundary fallback={<div>Something went wrong</div>}>
                 {!isHidden && (
-                    <ReactMapGLMap />
+                    <MapSlot />
                 )}
             </ErrorBoundary>
         </>
     );
 };
 
-root.render(<App />);
+root.render(<App type="mapbox" />);
